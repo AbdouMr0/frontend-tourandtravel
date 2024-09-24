b883eabab9839c6240362542f6ebcac11c4b5086
AbdouMr1 <jhonmith47@gmail.com>
2024-09-24 23:45:05 +0200

    modify app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Correct import
+import { BrowserRouter as  Routes, Route } from 'react-router-dom'; // Correct import
 import './App.css';
 import Home from './routes/Home';
 import Contact from './routes/Contact';
